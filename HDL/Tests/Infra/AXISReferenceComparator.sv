9f34941245e26461c1d9189fab767e6d16072c2e
anonsachin <sachin94roxz@gmail.com>
2024-11-26 23:20:46 +0530

    re-write of test comparator block.

--- HDL/Tests/Infra/AXISReferenceComparator.sv
+++ HDL/Tests/Infra/AXISReferenceComparator.sv
@@ -13,54 +13,33 @@ module AXISReferenceComparator #(
   AXI4S.Slave in 
 );
 ///////////////////////////////////////////////////////////////////////
-// Load the file into memory
+// reference source
 ///////////////////////////////////////////////////////////////////////
-localparam MEMORY_WIDTH = (1<<ADDR_WIDTH) - 1;
-localparam KEEP_WIDTH = (DATA_WIDTH + 7)/8;
-logic [DATA_WIDTH - 1:0] data [MEMORY_WIDTH - 1:0];
-initial 
-begin
-  $readmemh(SOURCE_FILE, data, 0 ,LIMIT - 1);
-end
+AXI4S #(.DATA_WIDTH(DATA_WIDTH)) ideal();
 
-///////////////////////////////////////////////////////////////////////
-// reading the data
-///////////////////////////////////////////////////////////////////////
-logic [ADDR_WIDTH:0] read_pointer;
+AXISSource #(
+  .DATA_WIDTH  (DATA_WIDTH ),
+  .ADDR_WIDTH  (ADDR_WIDTH ),
+  .LIMIT       (LIMIT      ),
+  .SOURCE_FILE (SOURCE_FILE)
+) reference (
+  .clk   (clk   ),
+  .resetn(resetn),
+  .out   (ideal   )
+);
 
-always_ff @ (posedge clk)
-begin
-if(resetn)
-begin
-  if ((read_pointer <= LIMIT) & (in.valid))
-    read_pointer <= read_pointer + 1;
-end
-else
-begin
-  read_pointer <= 0;
-end
-end
-
-assign in.ready = resetn;
 
 ///////////////////////////////////////////////////////////////////////
 // comparing the data
 ///////////////////////////////////////////////////////////////////////
 
-always_ff @ (posedge clk)
-begin
-if(resetn)
-begin
-  if(in.data == data[read_pointer])
-    test_pass <= test_pass & 1;
-  else
-    test_pass <= test_pass & 0;
-end
-else
-begin
-  test_pass <= 1;
-end
-end
+AXISComparator comparator (
+  .clk   (clk      ),
+  .resetn(resetn   ),
+  .in1   (in       ),
+  .in2   (ideal    ),
+  .equal (test_pass)
+);
 
 
 endmodule
