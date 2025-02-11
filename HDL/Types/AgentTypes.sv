c3656b9275e2cf2b1582e8fe225c33aa2d7ff727
anonsachin <sachin94roxz@gmail.com>
2025-02-11 13:25:20 +0530

    Added the seed config types.

--- HDL/Types/AgentTypes.sv
+++ HDL/Types/AgentTypes.sv
@@ -22,7 +22,7 @@ typedef struct packed {
 } GateConfig;
 
 typedef union packed {
-    GateConfig gateConfig;
+    GateConfig configValue;
     logic [GATE_CONFIG_IN_STREAM -1 :0] [DATA_WIDTH - 1:0] data;
 } GateConfigStore;
 
@@ -31,6 +31,23 @@ typedef union packed {
 function logic is_passthrough(input GateConfig gateConfig);
   return (gateConfig.aSelect == gateConfig.bSelect);
 endfunction
+
+localparam LFSR_SIZE = 16;
+localparam SEED_SIZE = 2*LFSR_SIZE;
+localparam SEED_CONFIG_IN_BYTES = (SEED_SIZE + (DATA_WIDTH - 1)) / DATA_WIDTH;
+localparam LFSR_CONFIG_COUNT_SIZE = $clog2(SEED_CONFIG_IN_BYTES);
+
+type logic [LFSR_SIZE - 1:0] lfsrType;
+
+typedef struct packed {
+  lfsrType gateSeed;
+  lfsrType wireSelectionSeed;
+} SearchConfig;
+
+typedef union packed {
+    SearchConfig configValue;
+    logic [SEED_CONFIG_IN_BYTES -1 :0] [DATA_WIDTH - 1:0] data;
+} SearchConfigStore;
 
 endpackage
 
