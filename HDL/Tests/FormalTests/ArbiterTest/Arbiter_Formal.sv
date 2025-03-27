00b2980c0009f862b05e942c3edb391dd19f3340
anonsachin <sachin94roxz@gmail.com>
2025-03-27 18:00:02 +0530

    Formal test added for arbiter.

---
+++ HDL/Tests/FormalTests/ArbiterTest/Arbiter_Formal.sv
@@ -0,0 +1,70 @@
+
+`include "Arbiter.sv"
+
+module Arbiter_Formal (
+  input wire clk,
+  input wire resetn,
+  input wire [2:0] req,
+  output wire [2:0] grant
+);
+
+/////////////////////////////////////////
+// DUT
+/////////////////////////////////////////
+
+Arbiter3 dut (
+  .clk(clk),
+  .resetn(resetn),
+  .req(req),
+  .grant(grant)
+);
+
+
+/////////////////////////////////////////
+// Asumptions
+/////////////////////////////////////////
+initial assume(resetn == 0);
+
+
+/////////////////////////////////////////
+// Assertions
+/////////////////////////////////////////
+
+always_ff @ (posedge clk)
+begin
+if(resetn)
+begin
+  ////////////////////////////
+  // Null Check
+  ////////////////////////////
+  if (req == 0)
+    assert(grant == 0);
+  ////////////////////////////
+  // Normal Operation
+  ////////////////////////////
+  if (req != 0)
+    assert($onehot(grant));
+  ////////////////////////////
+  // Transition Check
+  ////////////////////////////
+  if(((req == 3'b100 ) |(req == 3'b010 )|(req == 3'b001 ) | (req == 0)) & req == $past(req) & $stable(resetn))
+    assert(grant == $past(grant));
+  ////////////////////////////
+  // No 1
+  ////////////////////////////
+  if(req[0] == 1'b0 )
+    assert(grant != 3'b001);
+  ////////////////////////////
+  // No 2
+  ////////////////////////////
+  if(req[1] == 1'b0 )
+    assert(grant != 3'b010);
+  ////////////////////////////
+  // No 3
+  ////////////////////////////
+  if(req[2] == 1'b0 )
+    assert(grant != 3'b100);
+end
+end
+
+endmodule
